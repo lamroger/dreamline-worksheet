9cc83142d4ce82237d0a159bdd32ac213a971b9a
Roger Lam <mrlamroger@gmail.com>
2016-11-15 13:40:40 -0800

    remove unnecessary styling and fix testswith chai and enzyme

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './index';
+import { shallow } from 'enzyme';
+import {expect} from 'chai';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from './index';
+import MainContainer from '../MainContainer'
+import DreamlineCard from '../DreamlineCard';
+import TimescaleCard from '../TimescaleCard';
+import NextStepsCard from '../NextStepsCard';
+
+it('renders one <TimescaleCard/> component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(TimescaleCard)).to.have.length(1);
 });
+
+it('renders seven <DreamlineCard/> component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(DreamlineCard)).to.have.length(7);
+});
+
